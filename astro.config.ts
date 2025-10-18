4418abe38e528d9d6948d915bb7843d96cda8813
xuxm0678 <xuxm0678@gmail.com>
2025-10-18 11:36:14 -0500

    Update astro.config.ts

--- astro.config.ts
+++ astro.config.ts
@@ -22,8 +22,8 @@ const whenExternalScripts = (items: (() => AstroIntegration) | (() => AstroInteg
   hasExternalScripts ? (Array.isArray(items) ? items.map((item) => item()) : [items()]) : [];
 
 export default defineConfig({
-  site: 'https://xuxm0678.github.io/',
-  base: '/',
+  site: 'https://xuxm0678.github.io/xiaomengxu.github.io/',
+  base: '/xiaomengxu.github.io/',
   outDir: './dist',
   output: 'static',
 
