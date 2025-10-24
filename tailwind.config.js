d264ed5257ffb878184d4d0347b7bcc523a60d2b
xuxm0678 <xuxm0678@gmail.com>
2025-10-24 12:20:49 -0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ import plugin from 'tailwindcss/plugin';
 import typographyPlugin from '@tailwindcss/typography';
 
 export default {
+  darkMode: ['class', '[data-theme="dark"]'],
   content: ['./src/**/*.{astro,html,js,jsx,json,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
   	extend: {
@@ -92,5 +93,4 @@ export default {
     }),
       require("tailwindcss-animate")
 ],
-  darkMode: ['class', '[data-theme="dark"]'],
 };
